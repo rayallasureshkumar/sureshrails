35b41b7581839c371693dcb281afb2098da4b40b
rayallasureshkumar <rayallasureshkumar@gmail.com>
2017-11-03 21:45:09 +0530

    3rd commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,35 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if @user.save
+			puts "sssssssssssssssssssssssssssssssssssss"
 			redirect_to users_url
 		else 
 			render new_user_url
 		end
 	end
+	def index
+		@users = User.all
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+	def update
+		@user = User.find(params[:id])
+		@user.update_attributes(user_params)
+		redirect_to users_url
+	end
+	def destroy
+		@user = User.find(params[:id])
+		@user.destroy
+		redirect_to users_url
+		
+	end
 
 	private
 
 	def user_params
-		params.require(:user).permit(:all)
+		params.require(:user).permit!
 	end
+
 end
